82f363eef272fb40b40269a91989268fcb38e77d
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-07-26 16:52:12 -0300

    dev

--- apigateway.tf
+++ apigateway.tf
@@ -40,31 +40,6 @@ resource "aws_apigatewayv2_api" "hackathon-apigateway" {
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.hackathon-apigateway.id
-
-  name        = "lanchonete"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
-}
-
 resource "aws_apigatewayv2_integration" "http_proxy_integration_basic" {
   for_each = toset([
     "POST/doctors",
